8c9748f05163770d193ea43c8b3abcaa7bc32910
Denis Nikolas <design.by.niko.las@gmail.com>
2021-04-24 19:04:45 +0400

    Creating notifications

---
+++ app/Notifications/CompanyNewsCreated.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Notification;
+
+use App\Models\Company\CompanyNews;
+
+class CompanyNewsCreated extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(protected CompanyNews $news)
+    {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['broadcast', 'database'];
+    }
+
+	/**
+	 * Get the type of the notification being broadcast.
+	 *
+	 * @return string
+	 */
+	public function broadcastType()
+	{
+	    return 'company.news';
+	}
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return $this->news->toArray();
+    }
+}
